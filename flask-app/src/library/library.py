d71d2c9a1051015c9c630c3d8d259d440459e426
Stanley <stanleykywu@gmail.com>
2022-12-12 21:43:51 -0500

    library catalog lookup

--- flask-app/src/library/library.py
+++ flask-app/src/library/library.py
@@ -6,6 +6,24 @@ from src import db
 libraries = Blueprint("libraries", __name__)
 
 
+# Get all libraries in a country and town
+@libraries.route("/library_search/<country>/<town>", methods=["GET"])
+def get_libraries(country, town):
+    cursor = db.get_db().cursor()
+    cursor.execute(
+        f'SELECT name AS "Search_Results" from Library WHERE country = "{country}" AND town = "{town}"'
+    )
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = "application/json"
+    return the_response
+
+
 # Get all books that particular author has written
 @libraries.route("/libraries", methods=["GET"])
 def get_bookstores():
@@ -22,12 +40,31 @@ def get_bookstores():
     return the_response
 
 
-# Get all books of a library
-@libraries.route("/libraries/<libraryID>", methods=["GET"])
-def get_inventory(libraryID):
+# Search for books of a library
+@libraries.route(
+    "/book_catalog/<library_name>/<title>/<author_first>/<author_last>", methods=["GET"]
+)
+def get_inventory(library_name, title, author_first, author_last):
+    title_filter = (
+        f' AND title = "{title.replace("+", " ")}"' if title != "undefined" else ""
+    )
+    fname_filter = (
+        f' AND first_name = "{author_first.replace("+", " ")}"'
+        if author_first != "undefined"
+        else ""
+    )
+    lname_filter = (
+        f' AND last_name = "{author_last.replace("+", " ")}"'
+        if author_last != "undefined"
+        else ""
+    )
+
     cursor = db.get_db().cursor()
     cursor.execute(
-        f"SELECT * from BookInventory WHERE Library_ID = {libraryID} NATURAL JOIN Book"
+        f'SELECT isbn as "ISBN", title AS "Title", quantity AS "Number in Stock"  from BookInventory NATURAL JOIN Author NATURAL JOIN Book WHERE library_id = (SELECT library_id FROM Library WHERE name = "{library_name.replace("+", " ")}" LIMIT 1)'
+        + title_filter
+        + fname_filter
+        + lname_filter
     )
     row_headers = [x[0] for x in cursor.description]
     json_data = []
